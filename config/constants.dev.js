26999cf0597a5bb38185fb82eca6aad1ed16a8ef
chandramouli <codechandra@gmail.com>
2021-08-18 16:05:21 +0530

    changed dev.js

--- config/constants.dev.js
+++ config/constants.dev.js
@@ -2,8 +2,8 @@
 exports.MONGODB_URL="mongodb+srv//chandramouli:chandramouli@product.eiaje.mongodb.net/Product?retryWrites=true&w=majority?authSource=admin",
 exports.GOOGLE_CLIENT_ID="295953042537-56lnehrla46gc2d3l87dsc7n046ama2s.apps.googleusercontent.com",
 exports.GOOGLE_CLIENT_SECRET="68VkcNwtQQTZbOrHuKdWEi2O",
-exports.GOOGLE_CALLBACK_URL= "http://3.12.184.202:2000/login/google/callback",
+exports.GOOGLE_CALLBACK_URL= "http://localhost:2000/login/google/callback",
 exports.GITHUB_CLIENT_ID="0dd2b5f519bcda7fc9b9",
 exports.GITHUB_CLIENT_SECRET= "a4ed5ff3dd12879c8086cec562d3e58f00fa5b0c",
-exports.GITHUB_CALLBACK_URL="http://3.12.184.202:2000/login/github/callback"
+exports.GITHUB_CALLBACK_URL="http://localhost:2000/login/github/callback"
 
